e54846fb9e6a0fc5f40d916e40ca7e792700777b
skydoves <skydoves@naver.com>
2019-03-14 21:40:01 +0900

    implements getting superclass generic type of View from the annotatedElement

--- svc-compiler/src/main/java/com/naver/android/compiler/ControlTowerAnnotatedClass.java
+++ svc-compiler/src/main/java/com/naver/android/compiler/ControlTowerAnnotatedClass.java
@@ -1,24 +1,36 @@
 package com.naver.android.compiler;
 
 import com.google.common.base.VerifyException;
+import com.squareup.javapoet.ClassName;
 
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 
+@SuppressWarnings("WeakerAccess")
 public class ControlTowerAnnotatedClass {
 
   public final TypeElement annotatedElement;
   public final String packageName;
   public final String clazzName;
+  public final ClassName baseView;
+  public final String baseViewName;
 
   public ControlTowerAnnotatedClass(TypeElement annotatedElement, Elements elementUtils)
       throws VerifyException {
-
     PackageElement packageElement = elementUtils.getPackageOf(annotatedElement);
     this.packageName =
         packageElement.isUnnamed() ? null : packageElement.getQualifiedName().toString();
     this.annotatedElement = annotatedElement;
     this.clazzName = annotatedElement.getSimpleName().toString();
+
+    String[] superClazzSp0 = annotatedElement.getSuperclass().toString().split("<");
+    String[] superClazzSp1 = superClazzSp0[1].split(">");
+    String viewPackageSp = superClazzSp1[0];
+    int index = viewPackageSp.lastIndexOf(".");
+    if (index == -1) index = viewPackageSp.lastIndexOf("\\.");
+
+    this.baseViewName = viewPackageSp.substring(index + 1);
+    this.baseView = ClassName.get(viewPackageSp.substring(0, index), baseViewName);
   }
 }
