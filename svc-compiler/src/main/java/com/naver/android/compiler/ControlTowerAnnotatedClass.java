7f0cddcc254c4fbfa88b49042f39c850a47d6480
skydoves <skydoves@naver.com>
2019-03-15 20:48:20 +0900

    added @ControlTower annotation, implements ScreenExtendsGenerator

--- svc-compiler/src/main/java/com/naver/android/compiler/ControlTowerAnnotatedClass.java
+++ svc-compiler/src/main/java/com/naver/android/compiler/ControlTowerAnnotatedClass.java
@@ -1,7 +1,6 @@
 package com.naver.android.compiler;
 
 import com.google.common.base.VerifyException;
-import com.squareup.javapoet.ClassName;
 
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
@@ -13,8 +12,6 @@ public class ControlTowerAnnotatedClass {
   public final TypeElement annotatedElement;
   public final String packageName;
   public final String clazzName;
-  public final ClassName baseView;
-  public final String baseViewName;
 
   public ControlTowerAnnotatedClass(TypeElement annotatedElement, Elements elementUtils)
       throws VerifyException {
@@ -23,14 +20,5 @@ public class ControlTowerAnnotatedClass {
         packageElement.isUnnamed() ? null : packageElement.getQualifiedName().toString();
     this.annotatedElement = annotatedElement;
     this.clazzName = annotatedElement.getSimpleName().toString();
-
-    String[] superClazzSp0 = annotatedElement.getSuperclass().toString().split("<");
-    String[] superClazzSp1 = superClazzSp0[1].split(">");
-    String viewPackageSp = superClazzSp1[0];
-    int index = viewPackageSp.lastIndexOf(".");
-    if (index == -1) index = viewPackageSp.lastIndexOf("\\.");
-
-    this.baseViewName = viewPackageSp.substring(index + 1);
-    this.baseView = ClassName.get(viewPackageSp.substring(0, index), baseViewName);
   }
 }
