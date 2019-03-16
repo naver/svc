73bdc5f01f3238db43ccc1696306d387370013f2
Bansook Nam <bs.nam@navercorp.com>
2019-03-17 00:22:57 +0900

    add nonnull annotation

--- svc/src/main/kotlin/com/naver/android/svc/core/screen/SafeDialogFragment.java
+++ svc/src/main/kotlin/com/naver/android/svc/core/screen/SafeDialogFragment.java
@@ -1,12 +1,13 @@
 package com.naver.android.svc.core.screen;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 public class SafeDialogFragment extends DialogFragment {
 
     @Override
-    public void show(FragmentManager manager, String tag) {
+    public void show(@NonNull FragmentManager manager, String tag) {
         try {
             super.show(manager, tag);
         } catch (IllegalStateException e) {
